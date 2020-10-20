8dfc90f7590b3b1c0a739f631a5801eaf820d157
merima98 <merimaceranic98@gmail.com>
2020-10-20 13:35:10 +0200

    Adding changes to - Get posts per new date filter

--- src/posts/posts_controller.js
+++ src/posts/posts_controller.js
@@ -49,27 +49,34 @@ export async function getFilteredConfessions(req, res, next) {
     };
 
     const count = await Post.find({ approved: 1 }).countDocuments();
+    // const posts = await Post.find({ approved: 1 }) //only approved posts
+    //   .sort({ date: "desc" })
+    //   .skip(options.page * options.limit)
+    //   .limit(options.limit);
+    let posts;
+
     if (filter === "4") {
-      const posts = await Post.find({ approved: 1 }) //only approved posts
+      posts = await Post.find({ approved: 1 }) //only approved posts
         .sort({ date: "desc" })
         .skip(options.page * options.limit)
         .limit(options.limit);
-
-      const pagination = {
-        current_page: options.page,
-        total_item_count: count,
-        total_page: parseInt(count / options.limit),
-        next: {
-          page: (options.page += 1),
-        },
-      };
-      res
-        .status(200)
-        .send({ message: "Fetched posts successfully", posts, pagination });
     }
-  } catch {
-    const error = new Error("Needed to enter/click right value");
-    res.status(400).send(error);
+    if (filter !== "4") {
+      throw Error();
+    }
+    const pagination = {
+      current_page: options.page,
+      total_item_count: count,
+      total_page: parseInt(count / options.limit),
+      next: {
+        page: (options.page += 1),
+      },
+    };
+    res
+      .status(200)
+      .send({ message: "Fetched posts successfully", posts, pagination });
+  } catch (err) {
+    res.status(400).send({ message: "Needed to enter/click right value", err });
   }
 }
 
--- src/posts/posts_routes.js
+++ src/posts/posts_routes.js
@@ -9,12 +9,12 @@ import {
 
 const router = express.Router();
 
-router.post("/", create);
+router.post("/", create); //Adding a new confession
 
 router.get("/", getConfessions); //Get confessions in order of addition
 
-router.get("/sort/:filter", getFilteredConfessions);
+router.get("/sort/:filter", getFilteredConfessions); //Return confessions depending on the filter: 1 - random 2 - upvotes 3 - downvotes 4 - latest (by date)
 
-router.put("/:postId/:rate", rateConfession);
+router.put("/:postId/:rate", rateConfession); //Upvote or Downvote
 
 export default router;
